05b9bfa92bd4fae8d6b57a38ad0932a23c50b773
MarioLazarov15 <mmaarriioo2007@gmail.com>
2024-11-11 17:47:31 +0200

    Add project files.

---
+++ BookstoreApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection.Emit;
+
+namespace BookstoreApp.Infrastructure.Data
+{
+    public class ApplicationDbContext 
+    {
+    }
+}
