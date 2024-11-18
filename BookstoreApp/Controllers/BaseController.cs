4f2e495bef0488f40f4c42b7d068fda1a3ac7857
MarioLazarov15 <mmaarriioo2007@gmail.com>
2024-11-18 19:08:56 +0200

    Base controler and home/indexView added.

---
+++ BookstoreApp/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreApp.Controllers
+{
+    public class BaseController : Controller
+    {
+
+    }
+}
